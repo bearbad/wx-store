c322f21bf3c0bb9d4c26f9981a809fff3169be99
huai.li <xianbin01.liu@vipshop.com>
2019-06-10 17:02:42 +0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import UTIL from './util.js'
+import util from './util.js'
 import Modello from './modello.js'
 
 let WxModello = new Modello()
 
 const $page = decorator(Page, function (opt) {
-  UTIL.mixin(opt.data, WxModello.getCurrState(opt.modello))
-  const onLoad = opt.onLoad || function () {}
+  let { modello, data } = opt
+  let currState = WxModello.getCurrState(modello)
+  util.mixin(data, currState)
+  let onLoad = opt.onLoad || function () {}
   opt.onLoad = function (option) {
     opt.setData = function (params) {
       this.setData(params)
-      UTIL.mergeObj(opt.data, params)
+      util.mergeObj(data, params)
     }.bind(this)
     onLoad.apply(this, arguments)
   }
